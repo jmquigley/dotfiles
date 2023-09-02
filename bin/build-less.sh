13bd7a93823ce521d013f5ff9e4946cba67bffc7
James Quigley <jmquigley@outlook.com>
2023-09-02 16:24:43 +0000

    Fixed push/pop for temp directory in build-less.sh

--- bin/build-less.sh
+++ bin/build-less.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 VERSION=643
 
 pushd /tmp
+
 wget http://www.greenwoodsoftware.com/less/less-${VERSION}.tar.gz
 tar -xzf less-${VERSION}.tar.gz
 cd less-${VERSION}
 ./configure --prefix=/usr/local
 make
 make install
+
+popd
