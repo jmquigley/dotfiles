c3f4e8140ef47f5d899b8b98a60646d738423620
James Quigley <jmquigley@outlook.com>
2023-12-22 10:06:39 -0600

    Removed old powershell startup shell items

--- conf/profile.ps1
+++ conf/profile.ps1
@@ -2,25 +2,13 @@ function not-exist { -not (Test-Path $args) }
 Set-Alias !exist not-exist -Option "Constant, AllScope"
 Set-Alias exist Test-Path -Option "Constant, AllScope"
 
-Function Notepad {
-    & "C:\Program Files\Notepad++\notepad++.exe" $args
-}
-
-Function Environment {
-	Get-Item env
-}
-
-nal env Environment
 nal ll dir
-nal np Notepad
-nal w Goto-Workspace
 
 if (Test-Path $env:USERPROFILE\virtualenv\py3) {
 	. "$env:USERPROFILE\virtualenv\py3\Scripts\activate.ps1"
 }
 
 $env:DEBUG = "*"
-
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
