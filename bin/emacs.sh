dfc6c4882619c032b9b1324e96f1d9d58f288c78
jmquigley <jmquigley@outlook.com>
2025-05-15 10:50:07 -0500

    Fixed emacs wrapper script

--- bin/emacs.sh
+++ bin/emacs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S zsh --login
 
+TERM=xterm-256color
+
 #
 # An emacs wrapper to ensure that the zshenv is sourced when the
 # terminal is opened.
@@ -8,4 +10,4 @@
 [[ -f /root/.zshenv ]] && . /root/.zshenv
 [[ -f /root/.zshrc ]] && . /root/.zshrc
 
-/usr/bin/emacs
+/usr/bin/emacs-nox
