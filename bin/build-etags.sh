508e28648e4e1801e8857f8d2e22e5c67f57a2d6
jmquigley <jmquigley@outlook.com>
2025-05-20 10:44:41 -0500

    Created a path variable for TAGFILE

--- bin/build-etags.sh
+++ bin/build-etags.sh
@@ -4,4 +4,7 @@
 # Builds a database of etags from the current project repository
 #
 
- find . -name "*.cpp" -print -or -name "*.h" -print | xargs etags --append --output=`realpath ~/.emacs.d/TAGS`
+TAGFILE=`realpath ~/.emacs.d/TAGS`
+echo "creating tags in ${TAGFILE}"
+
+find . -name "*.cpp" -print -or -name "*.h" -print | xargs etags --append --output=${TAGFILE}
